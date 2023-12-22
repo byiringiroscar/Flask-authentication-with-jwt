cd6851f0cbbe96f0c37856c378730a7d20b2351b
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 15:22:04 +0200

    wip

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.String(), primary_key=True, default=str(uuid4()))
+    id = db.Column(db.String(), primary_key=True, default=lambda: str(uuid4()))
     username = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=False)
     password = db.Column(db.Text(), nullable=False)
