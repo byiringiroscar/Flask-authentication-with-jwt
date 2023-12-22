bc65a3a53def1c69584bdd61659107f2849e50b4
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 13:49:52 +0200

    wip

--- auth.py
+++ auth.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from models import User
 
 auth_bp = Blueprint('auth', __name__)
 
 
 @auth_bp.post('/register')
 def register_user():
+    data = request.get_json()
+    username = data.get('username')
+    user = User.get_user_by_username(username=username)
+    if user is not None:
+        pass
+    else:
+        return jsonify({"error": "User already exists"})
+    email = data.get('email')
     return jsonify({'message': 'User created'})
