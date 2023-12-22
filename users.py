a266a644dbe6d23992f99720308d824ee4a96a95
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 15:24:06 +0200

    wip

--- users.py
+++ users.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request, jsonify
 from models import User
 from schemas import UserSchema
+from flask_jwt_extended import jwt_required
 
 user_bp = Blueprint('users', __name__)
 
 
 @user_bp.get('/all')
+@jwt_required()
 def get_all_users():
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=3, type=int)
