e51671ecca3c53ad9ef5c7e159c471ff1c88b864
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 15:38:05 +0200

    Errorhandling jwt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from extension import db, jwt
 from auth import auth_bp
 from users import user_bp
@@ -14,5 +14,28 @@ def create_app():
     # register blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(user_bp, url_prefix='/users')
+
+
+    #jwt error handles
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_data):
+        return jsonify({
+            'message': 'Token has expired',
+            'error': 'token_expired'
+        }), 401
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({
+            'message': 'Signature verification failed',
+            'error': 'invalid_token'
+        }), 401
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({
+            'message': 'Request does not contain an access token',
+            'error': 'authorization_required'
+        }), 401
     return app
 
