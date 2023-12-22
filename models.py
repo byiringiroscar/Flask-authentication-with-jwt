b0bac3d9cfd8649feb14c9f92d7f501cc9d0b932
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 13:46:24 +0200

    Update models.py

--- models.py
+++ models.py
@@ -23,3 +23,11 @@ class User(db.Model):
     @classmethod
     def get_user_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
