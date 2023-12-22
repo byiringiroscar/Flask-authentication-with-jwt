0c08e19496acef77f3405f1c1e8b12e188e3f4cb
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 13:45:19 +0200

    Update models.py

--- models.py
+++ models.py
@@ -19,3 +19,7 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+    @classmethod
+    def get_user_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
