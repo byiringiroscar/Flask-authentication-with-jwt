4c986d0ec9981f32c89394c3137471a325f22584
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 15:09:14 +0200

    Update users.py

--- users.py
+++ users.py
@@ -6,7 +6,7 @@ user_bp = Blueprint('users', __name__)
 
 @user_bp.get('/all')
 def get_all_users():
-    page = request.args.get('page', default=1)
+    page = request.args.get('page', default=1, type=int)
 
     users = User.query.paginate(
             page=page,
