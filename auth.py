e10389476bc39e0c0ddc328b62a68e84204de243
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 13:40:39 +0200

    Register blueprint

--- auth.py
+++ auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 auth_bp = Blueprint('auth', __name__)
 
 
 @auth_bp.post('/register')
-def register():
-    pass
+def register_user():
+    return jsonify({'message': 'User created'})
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from extension import db
+from auth import auth_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
+    # initialize extension
     db.init_app(app)
+
+
+    # register blueprints
+    app.register_blueprint(auth_bp, url_prefix='/auth')
     return app
 
