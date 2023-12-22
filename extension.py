fa2be99198dee81f1daafc328121c138931f5b67
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-22 14:29:14 +0200

    WIP

--- extension.py
+++ extension.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import jwt_manager
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
-jwt = jwt_manager()
+jwt = JWTManager()
